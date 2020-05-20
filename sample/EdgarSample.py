e57e923dbffa63fdb926e4ce16f9735fb189fc49
Matin Kamali <matin_kamali@yahoo.com>
2020-05-19 23:23:26 -1000

    Update EdgarSample.py

--- sample/EdgarSample.py
+++ sample/EdgarSample.py
@@ -3,6 +3,9 @@ from qtcurate.dataprocess import DataProcess, DictionaryType
 from time import sleep
 
 API_KEY = 'YOUR-API-KEY'
+Climate_Dictionary_Id = '....'
+Disruption_Dictionary_Id = '....'
+
 
 def wait_for_completion(index: str):
     percentage = 0
@@ -17,21 +20,22 @@ def wait_for_completion(index: str):
 
 def create_phrases_table(key: str):
     qtdict = QtDict(key, "test")
-    qtdict.name("Weather")
-    qtdict.add_entry("Weather", "weather conditions")
-    qtdict.add_entry("Weather", "climate change")
-    qtdict.add_entry("Weather", "global warming")
+    qtdict.name("Climate")
+    qtdict.add_entry("weather conditions", "Climate Conditions")
+    qtdict.add_entry("climate change", "Climate Change")
+    qtdict.add_entry("global warming", "Climate Change")
     dictionary1 = qtdict.create()
     qtdict.clear()
 
     qtdict.name("Disruption")
-    qtdict.add_entry("Disruption", "disruption")
-    qtdict.add_entry("Disruption", "revenue declined")
-    qtdict.add_entry("Disruption", "revenue increased")
+    qtdict.add_entry("business disruption", "Business Disruptions")
+    qtdict.add_entry("competition", "Competition")
     dictionary2 = qtdict.create()
     qtdict.clear()
 
-    print(f"{dictionary1['id']} {dictionary2['id']}")
+    Climate_Dictionary_Id = dictionary1['id']
+    Disruption_Dictionary_Id = dictionary2['id']
+ #   print(f"{dictionary1['id']} {dictionary2['id']}")
 
 # 1- Run once and use the dictionary IDs in future runs
 # create_phrases_table(API_KEY)
@@ -45,12 +49,12 @@ dp.title("Test 10-K labeling")
 # 3- Set data feed and query
 data_feeds = ["edgar_8k_full.json"]
 dp.sources(data_feeds)
-dp.query("2098,3197,4447,3116,4977")
+dp.query("1706524,1648636,1326089,1169561")
 
 # 4- Pass data dictionaries
 dic = QtDict(API_KEY, "test")
-dp.search_rule(dic.fetch("c8faa596-4f36-4fa8-818c-a89324fd92a7")["key"], DictionaryType.NONE)
-dp.search_rule(dic.fetch("2b4d483a-c2b4-4f1f-bf53-7a8c9a53d236")["key"], DictionaryType.NONE)
+dp.search_rule(dic.fetch(Climate_Dictionary_Id)["key"], DictionaryType.NONE)
+dp.search_rule(dic.fetch(Disruption_Dictionary_Id)["key"], DictionaryType.NONE)
 
 # 5- Run and block until finish
 data_process = dp.create()
