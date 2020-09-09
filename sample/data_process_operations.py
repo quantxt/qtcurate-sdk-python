58982dd7387ffdb4bc0f1f94db10d3512d6565e3
Matin Kamali <matin_kamali@yahoo.com>
2020-09-09 15:39:51 -0400

    Update data_process_operations.py

--- sample/data_process_operations.py
+++ sample/data_process_operations.py
@@ -13,47 +13,49 @@ from qtcurate.field import Field
 
 
 API_KEY = "YOUR-API-KEY"
-SEARCH_PDF = "resources/sample.pdf"
+DOCUMENT = "resources/sample.pdf"
 
-list_of_files = []
-# Initialise with api key
+# Initialize with api key
 Qt.init(API_KEY)
+
+# 1- Upload the sample document for processing
+list_of_documents = []
+document = Document()
+doc = document.create(DOCUMENT)
+list_of_documents.append(doc)
+
+# 2- Create vocabulary
 voc = Vocabulary()
-voc.name("Regular expressions")
-# Create entries of dictionaries
 voc.add_entry("Industrials")
 voc.add_entry("Quasi-Governments")
 voc.add_entry("Governments")
-# Create QtDict
 voc.name("Allocations (%)").create()
 
-document = Document()
-doc = document.create(SEARCH_PDF)
-list_of_files.append(doc)
-
+# 3- Create Extractor - Regex must have 1 capturing group
 extractor = Extractor()
 extractor.set_vocabulary(voc.get_id())
 extractor.set_validator("^ +(\\d[\\d\\.\\,]+\\d)")
 extractor.set_data_type(DataType.DOUBLE)
 
+# 4- Run
 dp = DataProcess()
-# Name the project
-dp.title("testPdf")
-
+dp.title("My parser job")
 dp.with_extractor(extractor)
-dp.withDocuments(list_of_files)
-# Run and block until finish
+dp.withDocuments(list_of_documents)
 dp.create()
+
+# 5- Wait to finish
 dp.wait_for_completion()
 
+# 6- Export Field results
 result = Result(dp.get_id())
-
-# Export raw results to XLSX
-result.result_xlsx_exporter("sample.xlsx")
-
 for i in result.read():
     field = Field(i)
     print(f"{field.get_search_id()} {field.get_title()}")
+    
+# 7- Export raw results to XLSX
+result.result_xlsx_exporter("sample.xlsx")
 
+# 8- Clean up
 voc.delete(voc.get_id())
 dp.delete(dp.get_id())
