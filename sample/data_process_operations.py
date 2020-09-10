1c7554747ca17e537ed33066ef5803f6b4ff246f
Matin Kamali <matin_kamali@yahoo.com>
2020-09-10 10:11:23 -0400

    Update data_process_operations.py

--- sample/data_process_operations.py
+++ sample/data_process_operations.py
@@ -13,7 +13,6 @@ from qtcurate.result import Field, FieldValues
 
 
 API_KEY = "YOUR-API-KEY"
-API_KEY = "a5334f7d-2aac-44b3-aefc-a25cd9dd7bec"
 DOCUMENT = "resources/sample.pdf"
 
 # Initialise with api key
