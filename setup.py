93c25491908fdd48e1279d07c8f38b22869625fd
zebinovac <mbjelanovic@gmail.com>
2020-09-16 20:12:37 +0200

    version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="qtcurate",
-    version="1.4.8",
+    version="2.0",
     description="Theia SDK Search and Data Extraction",
     long_description=long_description,
     long_description_content_type="text/markdown",
