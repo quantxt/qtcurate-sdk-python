a1fb2432d60e33ed503eaf60d4ba9823653c98b9
Matin Kamali <matin_kamali@yahoo.com>
2020-09-16 15:21:03 -0400

    Update vocabulary_operation.py

--- sample/vocabulary_operation.py
+++ sample/vocabulary_operation.py
@@ -4,20 +4,16 @@ from qtcurate.qt import Qt
 
 API_KEY = "YOUR-API-KEY"
 
-FILE_NAME = "resources/revenue.tsv"
+# Initialize with api key
+Qt.init(API_KEY)
 
-# Initialise with api key
-Qt.init(API_KEY, "test")
-
-vocabulary = Vocabulary()
-# Set name of the Vocabulary
 # Create entries of vocabulary
+vocabulary = Vocabulary()
 vocabulary.add_entry("Apple Inc.", "AAPL")
 vocabulary.add_entry("Amazon.com", "AMZN")
 vocabulary.entries({"str": "Alphabet Inc.", "category": "GOOG"})
 
 entry_voc = None
-tsv_voc = None
 # Create Vocabulary
 try:
     entry_voc = vocabulary.name("some name").add_entry("Apple Inc.", "AAPL").create()
@@ -29,28 +25,19 @@ print(entry_voc)
 print(vocabulary.fetch(vocabulary.get_id()))
 vocabulary.clear()
 
-# Create list of dictionaries from tsv file
-# Name the project
-vocabulary.name("revenue")
-vocabulary.source(FILE_NAME)
-# Create QtDict
-try:
-    tsv_voc = vocabulary.create()
-except Exception as e:
-    print(e)
-print(tsv_voc)
-
-# Fetch from known dictionary
+# Fetch from known vocabulary
 print(vocabulary.fetch(entry_voc.id))
 
-# Update vocabulary, name and entries are mandatory options
+# Update an existing vocabulary
 new_dic = Vocabulary()
 new_dic.add_entry("Lenovo", "Thinkpad")
 new_dic.name("updated name")
 res = new_dic.update(entry_voc.id)
 print(res.id)
 
-# List all existing dictionaries
+# List all existing vocabularies
 print(vocabulary.read())
+
+# Delete an existing vocabulary
 print(vocabulary.delete(entry_voc.id))
 print(entry_voc)
