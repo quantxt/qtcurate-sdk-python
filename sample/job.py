a691e79d71dbb6298308c677c5a4e1ba36d0ad5e
zebinovac <mbjelanovic@gmail.com>
2020-12-10 10:06:51 +0100

    job.py

--- sample/job.py
+++ sample/job.py
@@ -1,10 +1,7 @@
-import os
-import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir))
 from qtcurate.qt import Qt
 from qtcurate.result import Result
 
-API_KEY = "e12eff3c-dec2-477a-8bc2-c923fce4f851"
+API_KEY = "YOUR_API_KEY"
 DOCUMENT = "resources/Bahia.xls"
 MODEL_ID = "cigkzgmxhp"
 Qt.init(API_KEY)
